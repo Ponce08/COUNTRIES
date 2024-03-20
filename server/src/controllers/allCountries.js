7ee7a9b1008ab7a3b470e6f0a474f161a2242440
ponce08 <yonathanponce2019@gmail.com>
2024-03-20 08:24:29 -0500

    modificado

--- server/src/controllers/allCountries.js
+++ server/src/controllers/allCountries.js
@@ -1,26 +1,26 @@
 const { Country, Activity } = require('../db');
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 
-const allCountries = async(req, res)=>{
-    try {
-        const { name } = req.query;
-        if(name){
-            const nameCountry = await Country.findAll({ 
-                where: { 
-                    name: {
-                        [Op.like]: `${name}%`
-                    }
-                }, include: { model: Activity}
-            })
-            return res.json(nameCountry)
-        };
-
-        const countriesBDD = await Country.findAll({ include:{model:Activity}}); 
-        return res.json(countriesBDD);
-
-    } catch (error) {
-        return res.status(500).send(error.message)
+const allCountries = async (req, res) => {
+  try {
+    const { name } = req.query;
+    if (name) {
+      const nameCountry = await Country.findAll({
+        where: {
+          name: {
+            [Op.like]: `${name}%`
+          }
+        },
+        include: { model: Activity }
+      });
+      return res.json(nameCountry);
     }
+
+    const countriesBDD = await Country.findAll({ include: { model: Activity } });
+    return res.json(countriesBDD);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = allCountries;
